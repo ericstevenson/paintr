209eca19c783c45801b233f033a777fd920c59e2
Eric Stevenson <esteve5@uwo.ca>
2015-03-26 17:07:55 -0400

    Implemented undo/redo with memento design pattern

--- js/paintr.js
+++ js/paintr.js
@@ -1,8 +1,140 @@
 // app namespace
 var paintr = paintr || {};
 
+/**
+ * Memento stores state for undo redo (memento design pattern)
+ * @param canvas the canvas (current state of the diagram)
+ * @constructor
+ */
+paintr.Memento = function(canvas) {
+  var serialized_canvas = JSON.stringify(canvas);
+  this.state = serialized_canvas;
+};
+
+/**
+ * Originator keeps track of the current state of the canvas (memento design pattern)
+ * @constructor
+ */
+paintr.Originator = function() {};
+
+/**
+ * Caretaker keeps track of states needed for undo and redo (memento design pattern)
+ * @constructor
+ */
+paintr.Caretaker = function() {
+  this.undo_stack = [];
+  this.redo_stack = [];
+};
+
+/**
+ * Returns the result of an undo action
+ * @returns {Memento}
+ */
+paintr.Caretaker.prototype.getUndoMemento = function() {
+  if (this.undo_stack.length >= 2) {
+    this.redo_stack.push(this.undo_stack.pop());
+    return this.undo_stack[this.undo_stack.length - 1];
+  } else {
+    return null;
+  }
+};
+
+/**
+ * Returns the result of a redo action
+ * @returns {Memento}
+ */
+paintr.Caretaker.prototype.getRedoMemento = function() {
+  if (this.redo_stack.length != 0) {
+    var memento = this.redo_stack.pop();
+    this.undo_stack.push(memento);
+    return memento;
+  } else {
+    return null;
+  }
+};
+
+/**
+ * Inserts a new memento into the undo stack. Called when actions occur
+ * @param canvas the current canvas
+ */
+paintr.Caretaker.prototype.insertMemento = function(canvas) {
+  if (canvas) {
+    var memento = new paintr.Memento(canvas);
+    this.undo_stack.push(memento);
+    this.redo_stack = [];
+  }
+};
+
+/**
+ * Checks to see if undo operation is possible
+ * @returns {boolean}
+ */
+paintr.Caretaker.prototype.canUndo = function() {
+  return this.undo_stack.length >= 2;
+};
+
+/**
+ * Checks to see if the redo operation is possible
+ * @returns {boolean}
+ */
+paintr.Caretaker.prototype.canRedo = function() {
+  return this.redo_stack.length != 0;
+};
+
+/**
+ * UndoRedo handles undo and redo operations for paintr
+ * @constructor
+ */
+paintr.UndoRedo = function() {
+  this.caretaker = new paintr.Caretaker();
+  this.originator = new paintr.Originator();
+};
+
+/**
+ * Calls the caretaker insert with the current canvas
+ * @param canvas
+ */
+paintr.UndoRedo.prototype.insert = function(canvas) {
+  this.caretaker.insertMemento(canvas);
+};
+
+/**
+ * Gets the next memento from the undo stack
+ * @returns {Object} Returns the state of the memento which is a JSON representation of a canvas
+ */
+paintr.UndoRedo.prototype.undo = function() {
+  this.originator.state = this.caretaker.getUndoMemento();
+  return this.originator.state;
+};
+
+/**
+ * Gets the next memento from the redo stack
+ * @returns {Object} Returns the state of the memento which is a JSON representation of a canvas
+ */
+paintr.UndoRedo.prototype.redo = function() {
+  this.originator.state = this.caretaker.getRedoMemento();
+  return this.originator.state;
+};
+
+/**
+ * Checks if undo is possible
+ * @returns {boolean}
+ */
+paintr.UndoRedo.prototype.canUndo = function() {
+  return this.caretaker.canUndo();
+};
+
+/**
+ * Checks if redo is possible
+ * @returns {boolean}
+ */
+paintr.UndoRedo.prototype.canRedo = function() {
+  return this.caretaker.canRedo();
+};
+
 //app attributes
-paintr.clipboard = [];
+paintr.clipboard = []; // stores cut, copy, and paste items
+paintr.undo_redo_manager = new paintr.UndoRedo(); // Handles undo redo operations for app
 
 /**
  * Handler for drawing rectangles and squares
@@ -268,6 +400,16 @@ paintr.paste = function() {
 };
 
 /**
+ * Handler for undo and redo - called every time an object is modified
+ * @param e - event
+ */
+paintr.undoRedoHandler = function (e) {
+  var object = e.target;
+  paintr.undo_redo_manager.insert(paintr.canvas);
+};
+
+
+/**
  * Handler for cutting and pasting
  * @param event
  */
@@ -297,11 +439,28 @@ paintr.onKeyDownHandler=function(event) {
         paintr.paste();
       }
       break;
+    case 89: // Redo (Ctrl+Y)
+      if (event.ctrlKey) {
+        event.preventDefault();
+        if (paintr.undo_redo_manager.canRedo()) {
+          var memento = paintr.undo_redo_manager.redo();
+          paintr.canvas.loadFromJSON(memento.state);
+        }
+      }
+      break;
+    case 90: // Undo (Ctrl+Z)
+      if (event.ctrlKey) {
+        event.preventDefault();
+        if (paintr.undo_redo_manager.canUndo()) {
+          var memento = paintr.undo_redo_manager.undo();
+          paintr.canvas.loadFromJSON(memento.state);
+        }
+      }
+      break;
     default:
       break;
   }
 };
-
 
 /**
  * Clears the canvas of all objects
@@ -338,6 +497,10 @@ paintr.save = function() {
 // Setup the canvas
 window.onload = function() {
   paintr.canvas = new fabric.Canvas('canvas');
+  paintr.undo_redo_manager.insert(paintr.canvas); // Store the blank canvas for undo redo
+  paintr.canvas.on("object:modified", paintr.undoRedoHandler);
+  //paintr.canvas.on("object:added", paintr.undoRedoHandler);
+  //paintr.canvas.on("object:removed", paintr.undoRedoHandler);
   paintr.canvas.backgroundColor = 'white';
   paintr.pen_color = 'black';
   paintr.mode = 'select';
