0fab20c8a85fc6f8fc7b2ded1295422a9dd9ae31
Eric Stevenson <esteve5@uwo.ca>
2015-04-07 15:16:23 -0400

    Added screenshots

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var favicon        = require('serve-favicon');
 // configuration ===========================================
 var port = process.env.PORT || 8180; // set our port
 app.use(favicon(__dirname + '/public/assets/logo.png'));
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
 
 
 // launch ======================================================================
